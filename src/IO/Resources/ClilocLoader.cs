401328f9c167dc63c54071341324979f6ec99b6a
Sghirate <frederick@parotat.de>
2022-08-01 21:57:55 +0200

    Make Cliloc.enu optional
    Support for single-language shards

--- src/IO/Resources/ClilocLoader.cs
+++ src/IO/Resources/ClilocLoader.cs
@@ -93,44 +93,45 @@ namespace ClassicUO.IO.Resources
                     }
 
                     if (string.Compare(_cliloc, "cliloc.enu", StringComparison.InvariantCultureIgnoreCase) != 0)
-                    { 
+                    {
                         string enupath = UOFileManager.GetUOFilePath("Cliloc.enu");
-
-                        using (BinaryReader reader = new BinaryReader(new FileStream(enupath, FileMode.Open, FileAccess.Read)))
+                        if (File.Exists(enupath))
                         {
-                            reader.ReadInt32();
-                            reader.ReadInt16();
-
-                            byte[] buffer = System.Buffers.ArrayPool<byte>.Shared.Rent(1024);
-
-                            try
+                            using (BinaryReader reader = new BinaryReader(new FileStream(enupath, FileMode.Open, FileAccess.Read)))
                             {
-                                while (reader.BaseStream.Length != reader.BaseStream.Position)
+                                reader.ReadInt32();
+                                reader.ReadInt16();
+
+                                byte[] buffer = System.Buffers.ArrayPool<byte>.Shared.Rent(1024);
+
+                                try
                                 {
-                                    int number = reader.ReadInt32();
-                                    byte flag = reader.ReadByte();
-                                    int length = reader.ReadInt16();
-
-                                    if (length > buffer.Length)
+                                    while (reader.BaseStream.Length != reader.BaseStream.Position)
                                     {
-                                        System.Buffers.ArrayPool<byte>.Shared.Return(buffer);
+                                        int number = reader.ReadInt32();
+                                        byte flag = reader.ReadByte();
+                                        int length = reader.ReadInt16();
 
-                                        buffer = System.Buffers.ArrayPool<byte>.Shared.Rent((length + 1023) & ~1023);
+                                        if (length > buffer.Length)
+                                        {
+                                            System.Buffers.ArrayPool<byte>.Shared.Return(buffer);
+
+                                            buffer = System.Buffers.ArrayPool<byte>.Shared.Rent((length + 1023) & ~1023);
+                                        }
+
+                                        reader.Read(buffer, 0, length);
+                                        string text = string.Intern(Encoding.UTF8.GetString(buffer, 0, length));
+
+                                        _entries[number] = text;
                                     }
-
-                                    reader.Read(buffer, 0, length);
-                                    string text = string.Intern(Encoding.UTF8.GetString(buffer, 0, length));
-
-                                    _entries[number] = text;
                                 }
-                            }
-                            finally
-                            {
-                                System.Buffers.ArrayPool<byte>.Shared.Return(buffer);
+                                finally
+                                {
+                                    System.Buffers.ArrayPool<byte>.Shared.Return(buffer);
+                                }
                             }
                         }
                     }
-
                     using (BinaryReader reader = new BinaryReader(new FileStream(path, FileMode.Open, FileAccess.Read)))
                     {
                         reader.ReadInt32();
